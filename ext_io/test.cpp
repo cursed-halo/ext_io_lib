048fc3a9f069499a32eba000cfc16bbf2dfdd8ca
cursed-halo <r.bhattacharya.2k@gmail.com>
2022-04-12 13:35:00 +0530

    removed unneeded headers in test.cpp

--- ext_io/test.cpp
+++ ext_io/test.cpp
@@ -1,20 +1,21 @@
 // test.cpp
 // created and maintained by @cursed-halo
-#include <iostream>
-#include <string>
-#include "ext_io.h"
+// #include <iostream> // not needed
+#include <string> // must include - causes string to skip otherwise
+#include "ext_io.h" // include in CURRENT directory
 
-using std::cout, std::cin, std::string;
+// using std::cout, std::cin, std::string;
 
+// example use cases
 int main(void) {
-	
-	int n = getInt("Enter Int : ");
-	float f = getFloat("Enter Float : ");
-	char c = getChar("Enter Char : ");
+	int n = getInt();
+	float f = getFloat();
+	char c = getChar();
 	string s = getString();
 	bool b = getBool();
 	
 	cout << "\nPrinting Values\n";
 	cout << n << "\n" << f << "\n" << c << "\n" << s << "\n" << b << "\n";
 	
+	return 0;
 }
