60e0808752c2b5dd175caf55933262dbe72d69f3
cursed-halo <r.bhattacharya.2k@gmail.com>
2022-04-15 01:17:48 +0530

    added getFloatArr()

--- ext_io/ext_io.h
+++ ext_io/ext_io.h
@@ -10,68 +10,79 @@ using std::cout, std::cin, std::string;
 #ifndef ext_io_h
 #define ext_io_h
 
-// const vars 
+// const vars
 const int MAXIMUM = 10000;
 
 // ===================regular input functions====================//
 
 // prompt for input, return int
-int getInt(const string m = "Enter Int : ") {
+int getInt(const string m = "Enter Int : ")
+{
 	int temp;
-	
+
 	cout << m;
 	cin >> temp;
-	
+
 	return temp;
 }
 
 // prompt for input, return float
-float getFloat(const string m = "Enter Float : ") {
+float getFloat(const string m = "Enter Float : ")
+{
 	float temp;
-	
+
 	cout << m;
 	cin >> temp;
-	
+
 	return temp;
 }
 
 // prompt for input, return char
-char getChar(const string m = "Enter Char : ") {
+char getChar(const string m = "Enter Char : ")
+{
 	char temp;
-	
+
 	cout << m;
 	cin >> temp;
-	
-	if(int(temp) >= 0 && int(temp) <= 127){
+
+	if (int(temp) >= 0 && int(temp) <= 127)
+	{
 		return temp;
-	} else {
+	}
+	else
+	{
 		return 'A'; // default case, may not work
 	}
 }
 
 // prompt for input, return string
-string getString(const string m = "Enter String : ") {
+string getString(const string m = "Enter String : ")
+{
 	string temp;
-	
+
 	cout << m;
-	cin.clear(); // clear immediate buffer
+	cin.clear();			   // clear immediate buffer
 	cin.ignore(MAXIMUM, '\n'); // ignore the next MAX characters upto \n
 	getline(cin, temp);
-	
+
 	return temp;
 }
 
 // prompt for input, return bool
-bool getBool(const string m = "Enter Bool : ") {
+bool getBool(const string m = "Enter Bool : ")
+{
 	bool temp;
-	
+
 	cout << m;
 	cin >> temp;
-	
+
 	// testing with if - convert to try catch
-	if(temp == 0 || temp == 1){
+	if (temp == 0 || temp == 1)
+	{
 		return temp;
-	} else {
+	}
+	else
+	{
 		return 1; // returns 1 if input is anything other than 0 or 1
 	}
 }
@@ -79,21 +90,46 @@ bool getBool(const string m = "Enter Bool : ") {
 // ===================array input functions====================//
 
 // prompt for input, return pointer to new <int>heap array
-int *getIntArr(int l = 5, const string m = "\nEnter Array Elements\n") {
-	if(l <= 0) {
+int *getIntArr(int l = 5, const string m = "\nEnter Array Elements\n")
+{
+	if (l <= 0)
+	{
 		cout << "\nError : Array Size <= 0\n";
 		l = 5; // set length to 5
-	} // pseudo error handling for length less than or equals to 0
-	
+	}		   // pseudo error handling for length less than or equals to 0
+
 	int *a = new int[l]; // creating array with length l on heap
-	
+
 	cout << m;
-	
-	for(int i = 0; i < l; i++) {
+
+	for (int i = 0; i < l; i++)
+	{
 		cout << "Element [" << i << "] = ";
 		cin >> a[i];
 	}
-	
+
+	return a; // return address of first element in array
+}
+
+// prompt for input, return pointer to new <float>heap array
+float *getFloatArr(int l = 5, const string m = "\nEnter Array Elements\n")
+{
+	if (l <= 0)
+	{
+		cout << "\nError : Array Size <= 0\n";
+		l = 5; // set length to 5
+	}		   // pseudo error handling for length less than or equals to 0
+
+	float *a = new float[l]; // creating array with length l on heap
+
+	cout << m;
+
+	for (int i = 0; i < l; i++)
+	{
+		cout << "Element [" << i << "] = ";
+		cin >> a[i];
+	}
+
 	return a; // return address of first element in array
 }
 
@@ -102,15 +138,18 @@ int *getIntArr(int l = 5, const string m = "\nEnter Array Elements\n") {
 // ===================array display functions====================//
 
 // display int array of length l
-void showIntArr(int *p, int l = 5) {
+void showIntArr(int *p, int l = 5)
+{
 	int *px = p; // hold address of p
-	
-	if(l <= 0) {
+
+	if (l <= 0)
+	{
 		cout << "\nError : Array Size <= 0\n";
 		l = 5; // set length to 5
-	} // pseudo error handling for length less than or equals to 0
-	
-	for(int i = 0; i < l; i++) {
+	}		   // pseudo error handling for length less than or equals to 0
+
+	for (int i = 0; i < l; i++)
+	{
 		cout << "E[" << i << "] = " << *px << "\tA[" << i << "] = " << px << "\n";
 		px++;
 	}
