9840d67f59244d3762027094d38e808ee0fdd054
cursed-halo <r.bhattacharya.2k@gmail.com>
2022-04-15 01:22:43 +0530

    added test cases for float array

--- ext_io/test.cpp
+++ ext_io/test.cpp
@@ -1,7 +1,7 @@
 // test.cpp
 // created and maintained by @cursed-halo
 // #include <iostream> // not needed
-#include <string> // for strings inside prompts
+#include <string>	// for strings inside prompts
 #include "ext_io.h" // include in CURRENT directory
 
 // using std::cout, std::cin, std::string;
@@ -9,20 +9,27 @@
 // example use cases
 int main(void) {
 	// for regular input types
-	
+
 	int n = getInt("Custom Prompts Allowed : ");
 	float f = getFloat("Otherwise Default Prompt Is : ");
 	char c = getChar("\"Enter Datatype :\"");
 	string s = getString();
 	bool b = getBool();
-	
+
 	cout << "\nPrinting Values\n";
-	cout << n << "\n" << f << "\n" << c << "\n" << s << "\n" << b << "\n";
-	
+	cout << n << "\n"
+		 << f << "\n"
+		 << c << "\n"
+		 << s << "\n"
+		 << b << "\n";
+
 	// for array related functions
-	
+
 	int *p = getIntArr(2);
 	showIntArr(p, 2);
 	
+	float *f = getFloatArr(2);
+	showFloatArr(f, 2);
+
 	return 0;
 }
