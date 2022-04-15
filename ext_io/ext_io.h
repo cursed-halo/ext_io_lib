a2aa9e35ece1c2f47f106b4c3dd727c291d3588f
cursed-halo <r.bhattacharya.2k@gmail.com>
2022-04-15 22:31:20 +0530

    added getCharArr() and showCharArr() : NOT WORKING AS INTENDED

--- ext_io/ext_io.h
+++ ext_io/ext_io.h
@@ -16,8 +16,7 @@ const int MAXIMUM = 10000;
 // ===================regular input functions====================//
 
 // prompt for input, return int
-int getInt(const string m = "Enter Int : ")
-{
+int getInt(const string m = "Enter Int : ") {
 	int temp;
 
 	cout << m;
@@ -27,8 +26,7 @@ int getInt(const string m = "Enter Int : ")
 }
 
 // prompt for input, return float
-float getFloat(const string m = "Enter Float : ")
-{
+float getFloat(const string m = "Enter Float : ") {
 	float temp;
 
 	cout << m;
@@ -38,26 +36,21 @@ float getFloat(const string m = "Enter Float : ")
 }
 
 // prompt for input, return char
-char getChar(const string m = "Enter Char : ")
-{
+char getChar(const string m = "Enter Char : ") {
 	char temp;
 
 	cout << m;
 	cin >> temp;
 
-	if (int(temp) >= 0 && int(temp) <= 127)
-	{
+	if (int(temp) >= 0 && int(temp) <= 127) {
 		return temp;
-	}
-	else
-	{
+	} else {
 		return 'A'; // default case, may not work
 	}
 }
 
 // prompt for input, return string
-string getString(const string m = "Enter String : ")
-{
+string getString(const string m = "Enter String : ") {
 	string temp;
 
 	cout << m;
@@ -69,20 +62,16 @@ string getString(const string m = "Enter String : ")
 }
 
 // prompt for input, return bool
-bool getBool(const string m = "Enter Bool : ")
-{
+bool getBool(const string m = "Enter Bool : ") {
 	bool temp;
 
 	cout << m;
 	cin >> temp;
 
 	// testing with if - convert to try catch
-	if (temp == 0 || temp == 1)
-	{
+	if (temp == 0 || temp == 1) {
 		return temp;
-	}
-	else
-	{
+	} else {
 		return 1; // returns 1 if input is anything other than 0 or 1
 	}
 }
@@ -90,10 +79,8 @@ bool getBool(const string m = "Enter Bool : ")
 // ===================array input functions====================//
 
 // prompt for input, return pointer to new <int>heap array
-int *getIntArr(int l = 5, const string m = "\nEnter Array Elements\n")
-{
-	if (l <= 0)
-	{
+int *getIntArr(int l = 5, const string m = "\nEnter Array Elements\n") {
+	if (l <= 0) {
 		cout << "\nError : Array Size <= 0\n";
 		l = 5; // set length to 5
 	}		   // pseudo error handling for length less than or equals to 0
@@ -102,8 +89,7 @@ int *getIntArr(int l = 5, const string m = "\nEnter Array Elements\n")
 
 	cout << m;
 
-	for (int i = 0; i < l; i++)
-	{
+	for (int i = 0; i < l; i++) {
 		cout << "Element [" << i << "] = ";
 		cin >> a[i];
 	}
@@ -112,10 +98,8 @@ int *getIntArr(int l = 5, const string m = "\nEnter Array Elements\n")
 }
 
 // prompt for input, return pointer to new <float>heap array
-float *getFloatArr(int l = 5, const string m = "\nEnter Array Elements\n")
-{
-	if (l <= 0)
-	{
+float *getFloatArr(int l = 5, const string m = "\nEnter Array Elements\n") {
+	if (l <= 0) {
 		cout << "\nError : Array Size <= 0\n";
 		l = 5; // set length to 5
 	}		   // pseudo error handling for length less than or equals to 0
@@ -124,8 +108,26 @@ float *getFloatArr(int l = 5, const string m = "\nEnter Array Elements\n")
 
 	cout << m;
 
-	for (int i = 0; i < l; i++)
-	{
+	for (int i = 0; i < l; i++) {
+		cout << "Element [" << i << "] = ";
+		cin >> a[i];
+	}
+
+	return a; // return address of first element in array
+}
+
+// prompt for input, return pointer to new <char>heap array
+char *getCharArr(int l = 5, const string m = "\nEnter Array Elements\n") {
+	if (l <= 0) {
+		cout << "\nError : Array Size <= 0\n";
+		l = 5; // set length to 5
+	}		   // pseudo error handling for length less than or equals to 0
+
+	char *a = new char[l]; // creating array with length l on heap
+
+	cout << m;
+
+	for (int i = 0; i < l; i++) {
 		cout << "Element [" << i << "] = ";
 		cin >> a[i];
 	}
@@ -138,39 +140,54 @@ float *getFloatArr(int l = 5, const string m = "\nEnter Array Elements\n")
 // ===================array display functions====================//
 
 // display int array of length l
-void showIntArr(int *p, int l = 5)
-{
+void showIntArr(int *p, int l = 5) {
 	int *px = p; // hold address of p
 
-	if (l <= 0)
-	{
+	if (l <= 0) {
 		cout << "\nError : Array Size <= 0\n";
 		l = 5; // set length to 5
 	}		   // pseudo error handling for length less than or equals to 0
 
-	for (int i = 0; i < l; i++)
-	{
+	for (int i = 0; i < l; i++) {
 		cout << "E[" << i << "] = " << *px << "\tA[" << i << "] = " << px << "\n";
 		px++;
 	}
 }
 
 // display float array of length l
-void showFloatArr(float *p, int l = 5)
-{
+void showFloatArr(float *p, int l = 5) {
 	float *px = p; // hold address of p
 
-	if (l <= 0)
-	{
+	if (l <= 0) {
 		cout << "\nError : Array Size <= 0\n";
 		l = 5; // set length to 5
 	}		   // pseudo error handling for length less than or equals to 0
 
-	for (int i = 0; i < l; i++)
-	{
+	for (int i = 0; i < l; i++) {
 		cout << "E[" << i << "] = " << *px << "\tA[" << i << "] = " << px << "\n";
 		px++;
 	}
+}
+
+// display char array of length l
+void showCharArr(char *p, int l = 5) {
+	char *px = p; // hold address of p
+
+	if (l <= 0) {
+		cout << "\nError : Array Size <= 0\n";
+		l = 5; // set length to 5
+	}		   // pseudo error handling for length less than or equals to 0
+
+	for (int i = 0; i < l; i++) {
+		cout << "E[" << i << "] = " << *px << "\tA[" << i << "] = " << px << "\n";
+		px++;
+	}
+	/* this is absolutely not working as intended 
+	 * expected output should have memory for pointer for ith value 
+	 * instead acting like a stack 
+	 * showing up as a triangle of inserted chars
+	 * don't use
+	 * /
 }
 
 // end of header
